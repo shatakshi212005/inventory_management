5d51cffc160dc102f38b0e85e73b4f84e98e71f4
atharvaVib29 <atharva.vib29@gmail.com>
2025-04-14 14:56:11 +0530

    Initial commit

---
+++ inventory/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
